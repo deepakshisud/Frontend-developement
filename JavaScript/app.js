55071f7a718b36ff86ee8ef1a015b6e6dac2792d
deepakshisud <deepakshisud@gmail.com>
2020-11-12 11:43:49 +0530

    Started Javascript

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,15 @@
 console.log("Hello from our first js file");
+let random = Math.random();
+if(random < 0.5) {
+    console.log("The number is less than 0.5");
+    console.log(random);
+}
+
+const password = prompt("please enter a new password");
+
+if(password.length>=6){
+    console.log("LONG ENOUGH");
+}
+else{
+    console.log("TOO SHORT");
+}
