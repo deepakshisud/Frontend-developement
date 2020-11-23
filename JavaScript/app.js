28b36633b09d1bc98d3ac16193743597e3a74d61
deepakshisud <deepakshisud@gmail.com>
2020-11-23 14:09:15 +0530

    Loops in JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,17 +5,29 @@ if(random < 0.5) {
     console.log(random);
 }
 
-const password = prompt("please enter a new password");
+//const password = prompt("please enter a new password");
 
-if(password.length>=6){
-    console.log("LONG ENOUGH");
-}
-else{
-    console.log("TOO SHORT");
-}
 
 
 console.log("Loops");
 for(let i=1;i<=10;i++){
     console.log(i);
 }
+
+const arr = ["dhg","yduygfuydg","ysdgyustguy","qewqeed","dyfyhsfhsgf"];
+for(let i = 0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+console.log("While loop");
+let count = 0;
+while(count<10){
+    console.log(`This is ${count}`);
+    count++;
+}
+
+const sec = "Deepakshi";
+const p = prompt("Enter the secret code");
+while(p!==sec){
+    p = prompt("Enter the secret code");
+}
+console.log("You got it!");
