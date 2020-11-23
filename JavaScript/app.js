ba7d731a8dcf39e809ca0318994eaf96d2d77236
deepakshisud <deepakshisud@gmail.com>
2020-11-23 14:15:21 +0530

    Loops in JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -9,25 +9,3 @@ if(random < 0.5) {
 
 
 
-console.log("Loops");
-for(let i=1;i<=10;i++){
-    console.log(i);
-}
-
-const arr = ["dhg","yduygfuydg","ysdgyustguy","qewqeed","dyfyhsfhsgf"];
-for(let i = 0; i<arr.length; i++){
-    console.log(arr[i]);
-}
-console.log("While loop");
-let count = 0;
-while(count<10){
-    console.log(`This is ${count}`);
-    count++;
-}
-
-const sec = "Deepakshi";
-const p = prompt("Enter the secret code");
-while(p!==sec){
-    p = prompt("Enter the secret code");
-}
-console.log("You got it!");
---
+++ JavaScript/loops.js
@@ -0,0 +1,33 @@
+// // console.log("Loops");
+// // for(let i=1;i<=10;i++){
+// //     console.log(i);
+// // }
+
+// // const arr = ["dhg","yduygfuydg","ysdgyustguy","qewqeed","dyfyhsfhsgf"];
+// // for(let i = 0; i<arr.length; i++){
+// //     console.log(arr[i]);
+// // }
+// // console.log("While loop");
+// // let count = 0;
+// // while(count<10){
+// //     console.log(`This is ${count}`);
+// //     count++;
+// // }
+
+// // const sec = "Deepakshi";
+// // const p = prompt("Enter the secret code");
+// // while(p!==sec){
+// //     p = prompt("Enter the secret code");
+// // }
+// // console.log("You got it!");
+
+let input = prompt("Hey say something");
+while(true)
+{
+    input = prompt(input);
+    if(input === "stop copying me"){
+        break;
+    }
+}
+console.log("OK YOU WIN!!!")
+
