d9395599af5452f0d233ca3cf8d27852beef45bf
deepakshisud <deepakshisud@gmail.com>
2020-11-27 17:17:16 +0530

    Array methods

--- JavaScript/array_methods.js
+++ JavaScript/array_methods.js
@@ -3,3 +3,16 @@ const num = [1,2,3,4,5];
 num.forEach(function(l){
     console.log(l)
 })
+
+
+//maps
+
+const dou =num.map(function(n){
+    return n*2;
+})
+
+//arrow functions
+
+const add = (x,y) => {
+    return x+y;
+}
