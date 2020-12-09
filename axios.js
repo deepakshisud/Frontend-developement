77ffbdc7d7202a2910146a3d69fac3df32e791ba
Deepakshi <deepakshisud@gmail.com>
2020-12-09 13:22:31 +0530

    Understanding axios

--- axios.js
+++ axios.js
@@ -1,7 +1,19 @@
-axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
-.then(res => {
-    console.log(res.data.ticker.price)
-})
-.catch(ree => {
-    console.log(err)
-})
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+// .then(res => {
+//     console.log(res.data.ticker.price)
+// })
+// .catch(ree => {
+//     console.log(err)
+// })
+
+const jokes = document.querySelector('#joke');
+const getDadJoke = async () => {
+    const conf = { headers : { Accept : 'application/json'}}
+    const res = await axios.get('https://icanhazdadjoke.com/',conf);
+    console.log(res);
+    const newLI = document.createElement('li');
+    newLI.append(res.data.joke);
+    jokes.append(newLI);
+
+}
+
