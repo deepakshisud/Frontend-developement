a9ea3cc0ff6b9204a7f2b0d87e1f490afe4541c1
Deepakshi <deepakshisud@gmail.com>
2020-12-09 12:32:33 +0530

    Fetch api

--- fetch-api.js
+++ fetch-api.js
@@ -10,3 +10,14 @@ fetch('https://api.cryptonator.com/api/ticker/btc-usd')
     .catch(err => {
         console.log(err)
     })
+
+const fetchPrice = async () => {
+    try{
+        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+        const data = await res.json();
+        console.log(data.ticker.price)
+    } catch(e) {
+        console.log("Something went wrong");
+    }
+    
+}
